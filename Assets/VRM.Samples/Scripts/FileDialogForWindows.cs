7bf65868a23a6aed16a34a036ec28075da69fe91
ousttrue <ousttrue@gmail.com>
2018-07-18 21:52:54 +0900

    Added VRMRuntimeExporterSample

--- Assets/VRM.Samples/Scripts/FileDialogForWindows.cs
+++ Assets/VRM.Samples/Scripts/FileDialogForWindows.cs
@@ -1,5 +1,6 @@
 ﻿#if UNITY_STANDALONE_WIN
 using System;
+using System.IO;
 using System.Runtime.InteropServices;
 #endif
 
@@ -37,12 +38,19 @@ namespace VRM
             public int reservedInt = 0;
             public int flagsEx = 0;
         }
+
         [DllImport("Comdlg32.dll", SetLastError = true, ThrowOnUnmappableChar = true, CharSet = CharSet.Auto)]
         public static extern bool GetOpenFileName([In, Out] OpenFileName ofn);
+        /*
         public static bool GetOpenFileName1([In, Out] OpenFileName ofn)
         {
             return GetOpenFileName(ofn);
         }
+        */
+
+        [DllImport("Comdlg32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern bool GetSaveFileName([In, Out] OpenFileName ofn);
+
         static string Filter(params string[] filters)
         {
             return string.Join("\0", filters) + "\0";
@@ -59,16 +67,42 @@ namespace VRM
             ofn.maxFileTitle = ofn.fileTitle.Length;
             ofn.initialDir = UnityEngine.Application.dataPath;
             ofn.title = title;
-            ofn.defExt = "PNG";
+            //ofn.defExt = "PNG";
             ofn.flags = 0x00080000 | 0x00001000 | 0x00000800 | 0x00000200 | 0x00000008;//OFN_EXPLORER|OFN_FILEMUSTEXIST|OFN_PATHMUSTEXIST| OFN_ALLOWMULTISELECT|OFN_NOCHANGEDIR
             if (!GetOpenFileName(ofn))
             {
                 return null;
-                //FileDialogResult("file:///" + ofn.file);
-            }        // later, possibly in some other method:
+            }
 
             return ofn.file;
+        }
+        public static string SaveDialog(string title, string path)
+        {
+            var extension = Path.GetExtension(path);
+            OpenFileName ofn = new OpenFileName();
+            ofn.structSize = Marshal.SizeOf(ofn);
+            ofn.filter = Filter("All Files", "*.*", extension, "*" + extension);
+            ofn.filterIndex = 2;
+            var chars = new char[256];
+            var it = Path.GetFileName(path).GetEnumerator();
+            for (int i = 0; i < chars.Length && it.MoveNext(); ++i)
+            {
+                chars[i] = it.Current;
+            }
+            ofn.file = new string(chars);
+            ofn.maxFile = ofn.file.Length;
+            ofn.fileTitle = new string(new char[64]);
+            ofn.maxFileTitle = ofn.fileTitle.Length;
+            ofn.initialDir = Path.GetDirectoryName(path);
+            ofn.title = title;
+            //ofn.defExt = "PNG";
+            ofn.flags = 0x00000002 | 0x00000004; // OFN_OVERWRITEPROMPT | OFN_HIDEREADONLY;
+            if (!GetSaveFileName(ofn))
+            {
+                return null;
+            }
 
+            return ofn.file;
         }
         #endregion
 #endif
---
+++ Assets/VRM.Samples/Scripts/VRMRuntimeExporter.cs
@@ -0,0 +1,109 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using VRM;
+
+
+public class VRMRuntimeExporter : MonoBehaviour
+{
+    [SerializeField]
+    Button m_loadButton;
+
+    [SerializeField]
+    Button m_exportButton;
+
+    GameObject m_model;
+
+    private void Awake()
+    {
+        m_loadButton.onClick.AddListener(OnLoadClicked);
+
+        m_exportButton.onClick.AddListener(OnExportClicked);
+    }
+
+    private void Update()
+    {
+        m_exportButton.interactable = (m_model != null);
+    }
+
+    #region Load
+    void OnLoadClicked()
+    {
+#if UNITY_STANDALONE_WIN
+        var path = FileDialogForWindows.FileDialog("open VRM", ".vrm");
+#else
+        var path = Application.dataPath + "/default.vrm";
+#endif
+        if (string.IsNullOrEmpty(path))
+        {
+            return;
+        }
+
+        var bytes = File.ReadAllBytes(path);
+        // なんらかの方法でByte列を得た
+
+        var context = new VRMImporterContext(path);
+
+        // GLB形式でJSONを取得しParseします
+        context.ParseVrm(bytes);
+
+
+        // metaを取得(todo: thumbnailテクスチャのロード)
+        var meta = context.ReadMeta();
+        Debug.LogFormat("meta: title:{0}", meta.Title);
+
+        // ParseしたJSONをシーンオブジェクトに変換していく
+        LoadAsync(context);
+    }
+
+    void LoadAsync(VRMImporterContext context)
+    {
+        var now = Time.time;
+        VRMImporter.LoadVrmAsync(context, go =>
+        {
+            var delta = Time.time - now;
+            Debug.LogFormat("LoadVrmAsync {0:0.0} seconds", delta);
+            OnLoaded(go);
+        });
+    }
+
+    void OnLoaded(GameObject root)
+    {
+        if (m_model != null)
+        {
+            GameObject.Destroy(m_model.gameObject);
+        }
+        m_model = root;
+
+        root.transform.rotation = Quaternion.Euler(0, 180, 0);
+    }
+    #endregion
+
+    #region Export
+    void OnExportClicked()
+    {
+#if UNITY_STANDALONE_WIN
+        var path = FileDialogForWindows.SaveDialog("save VRM", Application.dataPath + "/export.vrm");
+#else
+        var path = Application.dataPath + "/export.vrm";
+#endif
+        if (string.IsNullOrEmpty(path))
+        {
+            return;
+        }
+
+        Export(m_model, path);
+    }
+
+    void Export(GameObject go, string path)
+    {
+        Debug.LogFormat("export to {0}", path);
+        VRMExporter.Export(go, path, OnExported);
+    }
+
+    void OnExported(glTF_VRM vrm)
+    {
+        Debug.LogFormat("exported");
+    }
+    #endregion
+}
