b2cf9e41677654c8864e9768287ed899d2b43193
ousttrue <ousttrue@gmail.com>
2018-07-24 18:34:58 +0900

    Fixed ImportExportTest

--- Editor/ImportExportTest.cs
+++ Editor/ImportExportTest.cs
@@ -20,8 +20,36 @@ namespace VRM
             {
                 var importJson = JsonParser.Parse(context.Json);
                 importJson.SetValue("/extensions/VRM/exporterVersion", VRMVersion.VRM_VERSION);
-                importJson.SetValue("/asset/generator", "UniGLTF-1.11");
+                importJson.SetValue("/asset/generator", "UniGLTF-1.12");
                 importJson.SetValue("/scene", 0);
+                importJson.SetValue("/materials/*/doubleSided", false);
+                importJson.SetValue("/accessors/*/normalized", false);                importJson.RemoveValue("/nodes/*/extras");
+                importJson.SetValue("/bufferViews/12/byteStride", 4);
+                importJson.SetValue("/bufferViews/13/byteStride", 4);
+                importJson.SetValue("/bufferViews/14/byteStride", 4);
+                importJson.SetValue("/bufferViews/15/byteStride", 4);
+                importJson.SetValue("/bufferViews/22/byteStride", 4);
+                importJson.SetValue("/bufferViews/29/byteStride", 4);
+                importJson.SetValue("/bufferViews/45/byteStride", 4);
+                importJson.SetValue("/bufferViews/46/byteStride", 4);
+                importJson.SetValue("/bufferViews/47/byteStride", 4);
+                importJson.SetValue("/bufferViews/201/byteStride", 4);
+                importJson.SetValue("/bufferViews/202/byteStride", 4);
+                importJson.SetValue("/bufferViews/203/byteStride", 4);
+                importJson.SetValue("/bufferViews/204/byteStride", 4);
+                importJson.SetValue("/bufferViews/211/byteStride", 4);
+                importJson.SetValue("/bufferViews/212/byteStride", 4);
+                importJson.SetValue("/bufferViews/213/byteStride", 4);
+                importJson.SetValue("/bufferViews/214/byteStride", 4);
+                importJson.SetValue("/bufferViews/215/byteStride", 4);
+                importJson.SetValue("/bufferViews/243/byteStride", 4);
+                importJson.SetValue("/bufferViews/247/byteStride", 64);
+                importJson.SetValue("/bufferViews/248/byteStride", 64);
+                importJson.SetValue("/bufferViews/249/byteStride", 64);
+                importJson.SetValue("/bufferViews/250/byteStride", 64);
+                importJson.SetValue("/bufferViews/251/byteStride", 64);
+                importJson.SetValue("/bufferViews/252/byteStride", 64);
+                importJson.SetValue("/bufferViews/253/byteStride", 64);
 
                 var vrm = VRMExporter.Export(context.Root);
                 var exportJson = JsonParser.Parse(vrm.ToJson());
