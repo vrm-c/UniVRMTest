1702cc2d484922e749cf18b2fcb7d6ae68bd8ab3
PoChang-Su <39595967+PoChang-Su@users.noreply.github.com>
2018-10-24 19:18:18 +0900

    Updated codes related to VRMLookAtBlendShapeApplyer

--- Assets/VRMViewer/Scripts/EyeControlCamera.cs
+++ Assets/VRMViewer/Scripts/EyeControlCamera.cs
@@ -30,10 +30,12 @@ namespace VRMViewer
         private GameObject _vrmModel = null;
         private Transform _leftEye;
         private Transform _rightEye;
+        private bool _lookAtBone;
 
         public GameObject VrmModel { set { _vrmModel = value; } }
         public Transform LeftEye { set { _leftEye = value; } }
         public Transform RightEye { set { _rightEye = value; } }
+        public bool LookAtBone { set { _lookAtBone = value; } }
 
         private void Start()
         {
@@ -69,10 +71,20 @@ namespace VRMViewer
                     _vrmModel.GetComponent<VRMLookAtHead>().Target = _targetCamera.transform;
                 }
 
-                if (_vrmModel != null && (_vrmModel.GetComponent<VRMLookAtBoneApplyer>().LeftEye.Transform == null || _vrmModel.GetComponent<VRMLookAtBoneApplyer>().RightEye.Transform == null))
+                if (_lookAtBone)
                 {
-                    _vrmModel.GetComponent<VRMLookAtBoneApplyer>().LeftEye.Transform = _leftEye;
-                    _vrmModel.GetComponent<VRMLookAtBoneApplyer>().RightEye.Transform = _rightEye;
+                    if (_vrmModel != null && (_vrmModel.GetComponent<VRMLookAtBoneApplyer>().LeftEye.Transform == null || _vrmModel.GetComponent<VRMLookAtBoneApplyer>().RightEye.Transform == null))
+                    {
+                        _vrmModel.GetComponent<VRMLookAtBoneApplyer>().LeftEye.Transform = _leftEye;
+                        _vrmModel.GetComponent<VRMLookAtBoneApplyer>().RightEye.Transform = _rightEye;
+                    }
+                }
+                else
+                {
+                    if (_vrmModel != null && _vrmModel.GetComponent<VRMLookAtBlendShapeApplyer>().m_notSetValueApply)
+                    {
+                        _vrmModel.GetComponent<VRMLookAtBlendShapeApplyer>().m_notSetValueApply = false;
+                    }
                 }
 
                 _closeGameObject.DisableSphere();
